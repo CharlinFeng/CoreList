4871017c7a50c8e4d993b9ae97e5edc6d1a8f30b
时点软件 冯成林 <Charlin@fengchenglindeiMac.local>
2015-12-07 20:41:55 +0800

    更新顶部刷新出现的bug

--- CoreList/CoreList/Lib/Controller/CoreListCommonVC+Refresh.m
+++ CoreList/CoreList/Lib/Controller/CoreListCommonVC+Refresh.m
@@ -115,6 +115,14 @@ static NSString const *NoMoreDataMsg = @"没有更多数据了";
     });
     
     self.isRefreshData=NO;
+    
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        
+        if(![self.scrollView isKindOfClass:[UITableView class]]) return;
+        if(!self.hasData) return;
+        [(UITableView *)self.scrollView scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0] atScrollPosition:UITableViewRowAnimationTop animated:YES];
+    });
 }
 
 
